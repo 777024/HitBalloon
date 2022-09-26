267416416065a27b4e8e52bf9588629a338b93ab
777024 <tonyprince335@gmail.com>
2022-09-26 16:35:27 +0800

    add enemy movement

--- Assets/Scripts/autopilot.cs
+++ Assets/Scripts/autopilot.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class Autopilot : MonoBehaviour
 {
-    double velocity = 0;
+    Rigidbody2D enemyRigidbody;
+    Balloon balloon;
+    float velocity = 3;
+    Vector3 move;
+    float second;
     // Start is called before the first frame update
     void Start()
     {
-        Balloon balloon = gameObject.AddComponent(typeof(Balloon)) as Balloon;
+        balloon = gameObject.AddComponent(typeof(Balloon)) as Balloon;
+        enemyRigidbody = GetComponent<Rigidbody2D>();
+        enemyRigidbody.drag = 1;
+        move = transform.up * velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(balloon.balloonNumer == 1){
+            second -= Time.deltaTime;
+            if (second < 0)
+            {
+                if (Random.value > 0.5)
+                {
+                    for (float i = 0; i < 2; i += Time.deltaTime)
+                    {
+                        enemyRigidbody.AddForce(move);
+                    }
+                }else{
+                    second = 2;
+                }
+            }
+        }
     }
 }
