066973676234357c63399cf0fb53db70211303dd
777024 <tonyprince335@gmail.com>
2022-09-27 12:17:30 +0800

    enemy camera

--- Assets/Scripts/manager.cs
+++ Assets/Scripts/manager.cs
@@ -6,10 +6,19 @@ public class Manager : MonoBehaviour
 {
 
     // Start is called before the first frame update
+    public GameObject[] Enemies = null;
+    Vector3 enemyPos;
+    Vector3 viewPosEnemy;
+
+
     GameObject player;
     Vector3 playerPos;
+
+
     Camera cameraM;
-    Vector3 viewPos;
+    Vector3 viewPosPlayer;
+
+
     Vector3 leftBorder;
     Vector3 rightBorder;
     void Start()
@@ -19,26 +28,44 @@ public class Manager : MonoBehaviour
         leftBorder = new Vector3(cameraM.ViewportToWorldPoint(new Vector3(-0.03f, 0)).x, 0, 0);
         rightBorder = new Vector3(cameraM.ViewportToWorldPoint(new Vector3(1.03f, 0)).x, 0, 0);
     }
-    
+
     // Update is called once per frame
     void Update()
     {
         if (player != null)
         {
             playerPos = player.transform.position;
-            viewPos = cameraM.WorldToViewportPoint(playerPos);
-            if(viewPos.x < -0.03){
-                Debug.Log("out");
+            viewPosPlayer = cameraM.WorldToViewportPoint(playerPos);
+            if (viewPosPlayer.x < -0.03)
+            {
+                // Debug.Log("out");
                 // Destroy(player);
                 player.transform.localPosition += 2 * rightBorder;
-            }else if (viewPos.x > 1.03)
+            }
+            else if (viewPosPlayer.x > 1.03)
             {
-                Debug.Log("out");
+                // Debug.Log("out");
                 // Destroy(player);
                 player.transform.localPosition += 2 * leftBorder;
             }
         }
-        
-        
+
+        foreach (GameObject enemy in Enemies)
+        {
+            if (enemy != null)
+            {
+                enemyPos = enemy.transform.position;
+                viewPosEnemy = cameraM.WorldToViewportPoint(enemyPos);
+                if (viewPosEnemy.x < -0.03)
+                {
+                    enemy.transform.localPosition += 2 * rightBorder;
+                }
+                else if (viewPosEnemy.x > 1.03)
+                {
+                    // Debug.Log("Enemy out");
+                    enemy.transform.localPosition += 2 * leftBorder;
+                }
+            }
+        }
     }
 }
